3b934d8d54c69f163a8e5a18f8064003ce2720c9
Alok <alokchoudhary162@gmail.com>
2017-11-24 07:23:54 +0530

    e2e

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -3,12 +3,15 @@ import { AppPage } from './app.po';
 describe('protractor-e2e-testing App', () => {
   let page: AppPage;
 
+  
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('Should display title', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getTitle()).toEqual('end-end-testing');
   });
+
+
 });
